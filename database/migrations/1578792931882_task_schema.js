3a279fa95fe60fde2c7f892d2d4a2113930da9d3
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-01-11 22:48:41 -0300

    Criando models de projetos/tarefas

---
+++ database/migrations/1578792931882_task_schema.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class TaskSchema extends Schema {
+  up() {
+    this.create("tasks", table => {
+      table.increments();
+      table
+        .integer("project_id")
+        .unsigned()
+        .references("id")
+        .inTable("project")
+        .onUpdate("CASCADE")
+        .onDele("CASCADE")
+        .notNullabe();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDele("SET NULL");
+      table
+        .integer("file_id")
+        .unsigned()
+        .references("id")
+        .inTable("files")
+        .onUpdate("CASCADE")
+        .onDele("SET NULL");
+      table.string("title").notNullabe();
+      table.tex("description").notNullable();
+      table.timestamp("dure_date");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("tasks");
+  }
+}
+
+module.exports = TaskSchema;
